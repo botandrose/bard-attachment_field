a4ad70dca7a1db3b48111ccbd7a440d947cf4364
Micah Geisel <micah@botandrose.com>
2025-12-25 09:03:37 -0600

    fix bug.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope ActiveStorage.routes_prefix do
-    get "/blobs/info/:signed_id" => "bard/attachment_field/blobs#show", as: :rails_service_blob_info
+    get "/blobs/info/:signed_id" => "bard/attachment_field/blobs#show"
   end
 end
 
